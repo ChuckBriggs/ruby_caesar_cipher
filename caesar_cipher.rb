be6b75a93af4e32fef4f5d855a2365444f82bb7a
Charles Briggs <chucklistmail+site.github.com@gmail.com>
2020-10-09 01:12:06 -0600

    refactored for conciseness

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -19,22 +19,26 @@ def apply_cipher(val, n)
 end
 
 def convert_alpha_to_num(alpha_array)
-	num_array = alpha_array.map { |char| char.ord } 
-	return num_array
+	return alpha_array.map { |char| char.ord }
 end
 
 def convert_num_to_alpha(num_array)
-	alpha_array = num_array.map { |val| val.chr } 
-	return alpha_array
+	return num_array.map { |val| val.chr }
 end
 
 def caesar_cipher(message, cipher)
 	num_array = convert_alpha_to_num(message.split(""))
+	num_array.map! { |char| char = apply_cipher(char, cipher) }
+	return convert_num_to_alpha(num_array).join
 
-	coded_num_array = num_array.map { |char| char = apply_cipher(char, cipher) }
-	coded_alpha_array = convert_num_to_alpha(coded_num_array)
-
-	return coded_alpha_array.join
+##	orig_alpha_array = message.split("")
+##	orig_num_array = convert_from_alpha_to_num(orig_alpha_array)
+#	num_array = convert_alpha_to_num(message.split(""))
+#
+#	coded_num_array = num_array.map { |char| char = apply_cipher(char, cipher) }
+#	coded_alpha_array = convert_num_to_alpha(coded_num_array)
+#
+#	return coded_alpha_array.join
 end
 
 puts "Enter your message to encode: "
